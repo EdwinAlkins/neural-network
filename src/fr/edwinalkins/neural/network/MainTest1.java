758865aebd02754a0034c61d4d7a3d1195f277aa
William NAUROY <william.nauroy@etu.univ-nantes.fr>
2019-12-07 19:23:25 +0100

    [CODE] add test

--- src/fr/edwinalkins/neural/network/MainTest1.java
+++ src/fr/edwinalkins/neural/network/MainTest1.java
@@ -9,17 +9,16 @@ public class MainTest1 {
 
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
-		ReadDataFactory b = new ReadDataFactory("data1.csv","config1.csv");
+		ReadDataFactory b = new ReadDataFactory("data2.csv","config2.csv");
 		
 		BuildNetwork bn = new BuildNetwork(b.getConfigNetwork());
 		Reseaux r = bn.getNetwork();
 		r.setDataInput(b.getDataInput());
 		r.setDataOutput(b.getDataOutput());
 		
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < 80000; i++) {
 			r.learnAll();
 		}
-		//System.out.println(r);
 		for(ArrayList<Double> input:b.getDataInput()) {
 			System.out.println("===============");
 			System.out.println(input.toString());
