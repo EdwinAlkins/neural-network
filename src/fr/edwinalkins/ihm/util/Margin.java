fb798e053bd9db0917abd20263a307babcccc7b4
William NAUROY <william.nauroy@etu.univ-nantes.fr>
2019-12-07 19:25:30 +0100

    [CODE] add margin

---
+++ src/fr/edwinalkins/ihm/util/Margin.java
@@ -0,0 +1,16 @@
+package fr.edwinalkins.ihm.util;
+
+public final class Margin {
+
+	public final int left;
+	public final int right;
+	public final int up;
+	public final int bottom;
+	
+	public Margin(int l, int r, int u, int b) {
+		this.left = l;
+		this.right = r;
+		this.up = u;
+		this.bottom = b;
+	}
+}
---
+++ src/fr/edwinalkins/ihm/util/MarginDouble.java
@@ -0,0 +1,16 @@
+package fr.edwinalkins.ihm.util;
+
+public final class MarginDouble {
+
+	public final double left;
+	public final double right;
+	public final double up;
+	public final double bottom;
+	
+	public MarginDouble(double l, double r, double u, double b) {
+		this.left = l;
+		this.right = r;
+		this.up = u;
+		this.bottom = b;
+	}
+}
